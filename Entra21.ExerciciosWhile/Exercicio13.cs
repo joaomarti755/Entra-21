7d859c9f9f47c6e1efadd767d265228464dc5cab
João Vitor Altermann Martinelli <76072847+joaomarti755@users.noreply.github.com>
2022-05-10 16:39:24 -0300

    Continuando o Exerccio13 criando os inputs de informação e fazendo as funções

--- Entra21.ExerciciosWhile/Exercicio13.cs
+++ Entra21.ExerciciosWhile/Exercicio13.cs
@@ -11,6 +11,17 @@ namespace Entra21.ExerciciosWhile
         public void Executar()
         {
             int jogadores = 0;
+            double maiorAltura = double.MinValue;
+            string jogadorMaiorAltura = "";
+            double menorPeso = double.MaxValue;
+            string jogadorMenorPeso = "";
+            double maiorPeso = double.MinValue;
+            int menorQuantidadeCartoesAmarelos = int.MaxValue;
+            int maiorQuantidadeCatoesAmarelos = int.MinValue;
+            int maiorQuantidadeCartoesVermelhoa = int.MaxValue;
+            int menorQuantidadeCartoesVermelhos = int.MaxValue;
+            int quantidadeSexoF = 0;
+            int quantidadeSexoM = 0;
 
             while(jogadores < 22)
             {
@@ -19,6 +30,63 @@ namespace Entra21.ExerciciosWhile
 
                 Console.Write("Digite a idade: ");
                 int idade = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Digite o peso: ");
+                double peso = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Digite o sexo: ");
+                string sexo = Console.ReadLine().ToLower().Trim();
+
+                Console.Write("Digite a altura: ");
+                double altura = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Digite a quantidade de gols marcados: ");
+                int quantidadesGolsMarcados = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Digite a quantidade de cartões amarelos recebidos: ");
+                int quantidadeCartoesAmarelos = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Digite a quantidade de cartões vermelhos recebidos: ");
+                int quantidadeCartoesVermelhos = Convert.ToInt32(Console.ReadLine());
+
+                if(peso < menorPeso)
+                {
+                    jogadorMenorPeso = nome;
+                    menorPeso = peso;
+                }
+                if(altura > maiorAltura)
+                {
+                    jogadorMaiorAltura = nome;
+                    maiorAltura = altura;
+                }
+                if(sexo == "f")
+                {
+                    quantidadeSexoF = quantidadeSexoF + 1;
+                }
+                if(sexo == "m")
+                {
+                    quantidadeSexoM = quantidadeSexoM + 1;
+                }
+                if(quantidadeCartoesAmarelos < menorQuantidadeCartoesAmarelos)
+                {
+                    menorQuantidadeCartoesAmarelos = quantidadeCartoesAmarelos;
+                }
+                if(peso > maiorPeso)
+                {
+                    maiorPeso = peso;
+                }
+                if(quantidadeCartoesVermelhos < menorQuantidadeCartoesVermelhos)
+                {
+                    menorQuantidadeCartoesVermelhos = quantidadeCartoesVermelhos;
+                }
+                if(quantidadeCartoesAmarelos > maiorQuantidadeCatoesAmarelos)
+                {
+                    maiorQuantidadeCatoesAmarelos = quantidadeCartoesAmarelos;
+                }
+                if(quantidadeCartoesVermelhos < menorQuantidadeCartoesVermelhos)
+                {
+                    menorQuantidadeCartoesVermelhos = quantidadeCartoesVermelhos;
+                }
 
                 jogadores = jogadores + 1;
             }
