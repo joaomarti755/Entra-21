0edc24e96004236ddbc60edbe4fc411a023ef655
Amanda Caroline Bauler Mota <amanda.bauler@hotmail.com>
2022-05-13 21:52:01 -0300

    Inicio do ExercicioForEtry

---
+++ Entra-21.ExercicioForEtry/Exercicio01.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExercicioForEtry
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+            for(var i = 0; i < 13; i++)
+            {
+                Console.Write("Digite o nome da peça: ");
+                var nomePeca = Console.ReadLine();
+                var valorValido = false;
+                while(valorValido == false)
+                {
+                    try
+                    {
+                        Console.Write("Digite o preço da peça: ");
+                        var precoPeca = Convert.ToDouble(Console.ReadLine());
+
+                        if(precoPeca < 0)
+                        {
+                            Console.WriteLine("Valor digitado é invalido");
+                            valorValido = false;
+                        }
+                        else
+                        {
+                            valorValido = true;
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Entra-21.ExercicioForEtry/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Entra21.ExercicioForEtry;
+
+Console.WriteLine(@"-----------------MENU---------------
+1-Exercicio01");
+
+Console.Write("Digite a opção desejada: ");
+var opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+
+if(opcaoDesejada == 1)
+{
+    var exercicio01 = new Exercicio01();
+
+}
