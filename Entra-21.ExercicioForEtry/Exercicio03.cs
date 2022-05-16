cdb26b921de49a75386c277de8fc076095478696
João Vitor Altermann Martinelli <joaomarti755@gmail.com>
2022-05-16 20:34:37 -0300

    Criação e finalização do Exercicio03

---
+++ Entra-21.ExercicioForEtry/Exercicio03.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExercicioForEtry
+{
+    internal class Exercicio03
+    {
+        public void Executar()
+        {
+            Console.Write("Digite um numero: ");
+            var numero = Convert.ToInt32(Console.ReadLine());
+            var numeroTabuada = 0;
+            var textoTabuada = "";
+            for(var i = 0; i < 1000; i++)
+            {
+                ++numeroTabuada;
+                var valorTabuada = numeroTabuada * numero;
+
+                textoTabuada = textoTabuada + numeroTabuada + " x " + numero + " = " + valorTabuada + "\n";
+            }
+            Console.WriteLine(textoTabuada);
+        }
+    }
+}
--- Entra-21.ExercicioForEtry/Program.cs
+++ Entra-21.ExercicioForEtry/Program.cs
@@ -3,7 +3,8 @@ using Entra21.ExercicioForEtry;
 
 Console.WriteLine(@"-----------------MENU---------------
 1-Exercicio01
-2-Exercicio02");
+2-Exercicio02
+3-Exercicio03");
 
 Console.Write("Digite a opção desejada: ");
 var opcaoDesejada = Convert.ToInt32(Console.ReadLine());
@@ -17,4 +18,10 @@ else if(opcaoDesejada == 2)
 {
     var exercicio02 = new Exercicio02();
     exercicio02.Executar();
+}
+else if (opcaoDesejada == 3)
+{
+    var exercicio03 = new Exercicio03();
+    exercicio03.Executar();
+
 }
