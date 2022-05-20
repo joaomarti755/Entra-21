2a82f43c2323edac958481e83174a74146a2ecb3
joaomarti755 <joaomarti755@gmail.com>
2022-05-20 19:38:30 -0300

    Criação e finalização do Exercicio05

---
+++ Entra21.ExerciciosVetores/Exercicio05.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosVetores
+{
+    internal class Exercicio05
+    {
+        public void Executar()
+        {
+            double[] pesos = new double[5];
+            double somaPeso = 0;
+            for(var i = 0; i < 5; i++)
+            {
+                Console.Write("Digite o peso: ");
+                pesos[i] = Convert.ToDouble(Console.ReadLine());
+
+                somaPeso = somaPeso + pesos[i];
+            }
+            double mediaPeso = somaPeso / 4;
+
+            Console.WriteLine("A soma total dos pesos é: " + somaPeso);
+            Console.WriteLine("A média dos pesos é: " + mediaPeso);
+        }
+    }
+}
--- Entra21.ExerciciosVetores/Program.cs
+++ Entra21.ExerciciosVetores/Program.cs
@@ -5,7 +5,8 @@ Console.WriteLine(@"----------------MENU---------------------
 1-Exercicio01
 2-Exercicio02
 3-Exercicio03
-4-Exercicio04");
+4-Exercicio04
+5-Exercicio05");
 
 Console.Write("Digita a opção desejada: ");
 var opcaoDesejada = Convert.ToInt32(Console.ReadLine());
@@ -29,4 +30,9 @@ else if(opcaoDesejada == 4)
 {
     var exercicio04 = new Exercicio04();
     exercicio04.Executar();
+}
+else if(opcaoDesejada == 5)
+{
+    var exercicio05 = new Exercicio05();
+    exercicio05.Executar();
 }
