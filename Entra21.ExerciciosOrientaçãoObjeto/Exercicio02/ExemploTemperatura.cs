e2f559754aaf292cbc9c1cc0e1db3d9247a79de8
Joao.Martinelli <joaomarti755@gmail.com>
2022-05-27 20:27:20 -0300

    Inicio do exercicio02 começo da criação das Propriedades e Metodos

---
+++ Entra21.ExerciciosOrientaçãoObjeto/Exercicio02/ExemploTemperatura.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjeto.Exercicio02
+{
+    internal class ExemploTemperatura
+    {
+    }
+}
---
+++ Entra21.ExerciciosOrientaçãoObjeto/Exercicio02/Temperatura.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjeto.Exercicio02
+{
+    internal class Temperatura
+    {
+        public double TemperaturaOrigem;
+        public double TemperaturaDestino;
+        public double TemperaturaValor;
+
+        public double CalculoCelsiusParaKelvin()
+        {
+            var calcularCelsiusParaKelvin = TemperaturaValor + 273.15;
+
+            return calcularCelsiusParaKelvin;
+        }
+        
+        public double CalculoCelsiusParaFahrenheit()
+        {
+            var calcularCelasiusParaFahrenheit = (TemperaturaValor * 9 / 5) + 32;
+
+            return calcularCelasiusParaFahrenheit;
+        }
+
+        public double CalculoKelvinParaCelsius()
+        {
+            var calcularKelvinParaCelsius = TemperaturaValor - 273.15;
+
+            return calcularKelvinParaCelsius;
+        }
+
+        public double CalculoKelvinParaFahrenheit()
+        {
+            var calcularKelvinParaFahrenheit = (TemperaturaValor - 273.15) * 9 / 5 + 32;
+
+            return calcularKelvinParaFahrenheit;
+        }
+
+        public double CalculoFahrenheitParaCelsius()
+        {
+            var calcularFahrenheitParaCelsius = (TemperaturaValor - 32)
+        }
+
+    }
+}
