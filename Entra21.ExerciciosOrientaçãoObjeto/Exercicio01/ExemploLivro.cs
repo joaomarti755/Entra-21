5cf3bf6b17d4746c1698da5f1dd9216cd5e71613
joaomarti755 <joaomarti755@gmail.com>
2022-05-26 22:12:42 -0300

    Criação dos ExerciciosOrientaçãoObjeto foi criado o primeiro exercicio

---
+++ Entra21.ExerciciosOrientaçãoObjeto/Exercicio01/ExemploLivro.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjeto.Exercicio01
+{
+    public class ExemploLivro
+    {
+        public void Executar()
+        {
+            Livro livro = new Livro();
+            Console.WriteLine("Digite o título do livro: ");
+            livro.Titulo = Console.ReadLine().Trim();
+            Console.WriteLine("Digite o autor do livro: ");
+            livro.Autor = Console.ReadLine().Trim();
+            Console.WriteLine("Digite o idioma original: ");
+            livro.IdiomaOriginal = Console.ReadLine().Trim();
+            Console.WriteLine("Digite a data de lançamento: ");
+            livro.DataLancamento = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade de de páginas: ");
+            livro.QuantidadePaginas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade de paginas lidas: ");
+            livro.QuantidadePaginasLidas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade de releituras: ");
+            livro.QuantidadeReleitura = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(@$"O Titulo e o autor é: {livro.TituloComAutor()}
+A quantidade de paginas que faltam ler é: {livro.QuantidadePaginasParaLer()}
+");
+        }
+    }
+}
---
+++ Entra21.ExerciciosOrientaçãoObjeto/Exercicio01/Livro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjeto.Exercicio01
+{
+    public class Livro
+    {
+        public string Titulo;
+        public string Autor;
+        public string CodigoIsbn;
+        public string IdiomaOriginal;
+        public DateTime DataLancamento;
+        public int QuantidadePaginas;
+        public int QuantidadePaginasLidas;
+        public int QuantidadeReleitura;
+
+        public string TituloComAutor()
+        {
+            var tituloAutor = Titulo + "" + Autor;
+
+            return tituloAutor;
+        }
+        
+        public int QuantidadePaginasParaLer()
+        {
+            var paginasParaLer =  QuantidadePaginas - QuantidadePaginasLidas;
+
+            return paginasParaLer;
+        }
+
+        public int QuantidadePaginasLidasNoTotal()
+        {
+            var
+        }
+    }
+}
