dbc62b386451b660582ba59dbf56674c513bab41
João Vitor Altermann Martinelli <76072847+joaomarti755@users.noreply.github.com>
2022-05-30 16:46:52 -0300

    Criação de Tests

---
+++ Entra21.ExerciciosOrientacaoObjeto.Tests/Exercicio01/Livro.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjeto.Tests.Exercicio01
+{
+    internal class Livro
+    {
+    }
+}
