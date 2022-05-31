398e88329ab4f8c8e0c895587a0e932fa15c6da9
joaomarti755 <joaomarti755@gmail.com>
2022-05-30 22:08:35 -0300

    Criação do Exercicio03

---
+++ Entra21.ExerciciosOrientaçãoObjeto/Exercicio03/ConversorAscii.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjeto.Exercicio03
+{
+    public class ConversorAscii
+    {
+        public string Caracter;
+
+        public string ObterCodigoAsciiDoCaracter()
+        {
+            var caracter = Caracter
+        }
+    }
+}
