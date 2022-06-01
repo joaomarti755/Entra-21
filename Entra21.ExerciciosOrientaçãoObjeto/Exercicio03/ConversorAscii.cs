8d00fb04e7fe21ea7fa61888d68de53c4c208688
joaomarti755 <joaomarti755@gmail.com>
2022-06-01 19:06:17 -0300

    Criação de metodos do Exercicio04

--- Entra21.ExerciciosOrientaçãoObjeto/Exercicio03/ConversorAscii.cs
+++ Entra21.ExerciciosOrientaçãoObjeto/Exercicio03/ConversorAscii.cs
@@ -8,11 +8,67 @@ namespace Entra21.ExerciciosOrientacaoObjeto.Exercicio03
 {
     public class ConversorAscii
     {
+
         public string Caracter;
 
         public string ObterCodigoAsciiDoCaracter()
         {
-            var caracter = Caracter
+            string[] caracteres = new string[52];
+            caracteres[0] = "a";
+            caracteres[1] = "b";
+            caracteres[2] = "c";
+            caracteres[3] = "d";
+            caracteres[4] = "e";
+            caracteres[5] = "f";
+            caracteres[6] = "g";
+            caracteres[7] = "h";
+            caracteres[8] = "i";
+            caracteres[9] = "j";
+            caracteres[10] = "k";
+            caracteres[11] = "l";
+            caracteres[12] = "m";
+            caracteres[13] = "n";
+            caracteres[14] = "o";
+            caracteres[15] = "p";
+            caracteres[16] = "q";
+            caracteres[17] = "r";
+            caracteres[18] = "s";
+            caracteres[19] = "t";
+            caracteres[20] = "u";
+            caracteres[21] = "v";
+            caracteres[22] = "w";
+            caracteres[23] = "x";
+            caracteres[24] = "y";
+            caracteres[25] = "z";
+            caracteres[0] = "A";
+            caracteres[1] = "B";
+            caracteres[2] = "C";
+            caracteres[3] = "D";
+            caracteres[4] = "E";
+            caracteres[5] = "F";
+            caracteres[6] = "G";
+            caracteres[7] = "H";
+            caracteres[8] = "I";
+            caracteres[9] = "J";
+            caracteres[10] = "K";
+            caracteres[11] = "L";
+            caracteres[12] = "M";
+            caracteres[13] = "N";
+            caracteres[14] = "O";
+            caracteres[15] = "P";
+            caracteres[16] = "Q";
+            caracteres[17] = "R";
+            caracteres[18] = "S";
+            caracteres[19] = "T";
+            caracteres[20] = "U";
+            caracteres[21] = "V";
+            caracteres[22] = "W";
+            caracteres[23] = "X";
+            caracteres[24] = "Y";
+            caracteres[25] = "Z";
+
+            return caracteres[];
         }
+
     }
 }
