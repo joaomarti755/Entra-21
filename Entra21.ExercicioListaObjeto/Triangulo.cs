1736276ee0c79b801b1c7660d2749ebcdeaef548
joaomarti755 <joaomarti755@gmail.com>
2022-06-09 22:03:15 -0300

    Criação da classe Triangulo

---
+++ Entra21.ExercicioListaObjeto/Triangulo.cs
@@ -0,0 +1,22 @@
+﻿namespace Entra21.ExercicioListaObjeto
+{
+    internal class Triangulo
+    {
+        public int Lado1;
+        public int Lado2;
+        public int Lado3;
+        public int Codigo;
+
+        public bool ValidarTriangulo()
+        {
+            if((Lado1 + Lado2 > Lado3) || (Lado1 + Lado3 > Lado2) || (Lado2 + Lado3 > Lado1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
