c5baf7ecb229ac1f9b4e3e2dbb60e3efcfd4f949
joaomarti755 <joaomarti755@gmail.com>
2022-06-10 18:58:39 -0300

    Finalização da classe TrianguloServico

--- Entra21.ExercicioListaObjeto/TrianguloServico.cs
+++ Entra21.ExercicioListaObjeto/TrianguloServico.cs
@@ -4,6 +4,84 @@
     {
         private List<Triangulo> triangulos = new List<Triangulo>();
 
-        public bool Adicionar(int lado1, int lado2, int lado3, int codigo) { }
+        public bool Adicionar(int lado1, int lado2, int lado3)
+        {
+            Triangulo triangulo = new Triangulo();
+
+            triangulo.Lado1 = lado1;
+            triangulo.Lado2 = lado2;
+            triangulo.Lado3 = lado3;
+
+            int codigoAtual = 1;
+
+            if(triangulo.ValidarTriangulo() == true)
+            {
+
+            triangulo.Codigo = codigoAtual;
+
+            codigoAtual = codigoAtual + 1;
+
+            triangulos.Add(triangulo);  
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public bool Editar(int codigoParaAlterar, int codigo, int lado1, int lado2, int lado3)
+        {
+            Triangulo trianguloParaAlterar = ObterPorCodigo(codigoParaAlterar);
+
+            if(trianguloParaAlterar == null)
+            {
+                return false;
+            }
+
+            trianguloParaAlterar.Lado1 = lado1;
+            trianguloParaAlterar.Lado2 = lado2;
+            trianguloParaAlterar.Lado3 = lado3;
+
+            return false;
+        }
+
+        public bool Apagar(int codigo)
+        {
+            for(var i = 0; i < triangulos.Count; i++)
+            {
+                Triangulo triangulo = triangulos[i];
+
+                if(triangulo.Codigo == codigo)
+                {
+                    triangulos.Remove(triangulo);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public List<Triangulo> ObterTodos()
+        {
+            return triangulos;
+        }
+
+        public Triangulo ObterPorCodigo(int codigo)
+        {
+            for(var i = 0; i < triangulos.Count; i++)
+            {
+                var trianguloAtual = triangulos[i];
+
+                if(trianguloAtual.Codigo == codigo)
+                {
+                    return trianguloAtual;
+                }
+            }
+            return null;
+        }
+
     }
 }
