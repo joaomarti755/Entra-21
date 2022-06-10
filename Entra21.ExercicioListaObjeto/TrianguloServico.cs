36025f449fa60b69646fb439d3439e936120359b
joaomarti755 <joaomarti755@gmail.com>
2022-06-09 22:11:29 -0300

    Criação da classe TrianguloServico

---
+++ Entra21.ExercicioListaObjeto/TrianguloServico.cs
@@ -0,0 +1,9 @@
+﻿namespace Entra21.ExercicioListaObjeto
+{
+    internal class TrianguloServico
+    {
+        private List<Triangulo> triangulos = new List<Triangulo>();
+
+        public bool Adicionar(int lado1, int lado2, int lado3, int codigo) { }
+    }
+}
