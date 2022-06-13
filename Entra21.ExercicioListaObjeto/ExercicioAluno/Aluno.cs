a15c11ebad3676c95422eccf05709078d215f6b0
João Vitor Altermann Martinelli <76072847+joaomarti755@users.noreply.github.com>
2022-06-13 15:07:20 -0300

    Criação do Exercicio Aluno foi criado a classe alno, AlunoStatus e AlunoServico

---
+++ Entra21.ExercicioListaObjeto/ExercicioAluno/Aluno.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExercicioListaObjeto.ExercicioAluno
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public int CodigoMatricula;
+        public int idade;
+        public string MateriaFavorita;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double CalcularMedia()
+        {
+            var media = Nota1 + Nota2 + Nota3 / 3;
+
+            return media;
+        }
+
+        public AlunoStatus ObterStatus()
+        {
+            if(CalcularMedia() >= 7)
+            {
+                return AlunoStatus.Aprovado;
+            }
+            else if(CalcularMedia() >= 5)
+            {
+                return AlunoStatus.EmExame;
+            }
+            else
+            {
+                return AlunoStatus.Reprovado;
+            }
+        }
+    }
+}
