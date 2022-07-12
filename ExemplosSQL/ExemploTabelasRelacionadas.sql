75f77d176b2f145350531831b6ffae2e7730a12d
joaomarti755 <joaomarti755@gmail.com>
2022-07-11 22:08:33 -0300

    Aprendendo Tabelas relacionais em SQL

---
+++ ExemplosSQL/ExemploTabelasRelacionadas.sql
@@ -0,0 +1,125 @@
+﻿CREATE TABLE pecas(
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(50),
+	tipo INTEGER,
+	preco_unitario DECIMAL(6,2));
+
+CREATE TABLE clientes(
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(150),
+	cpf VARCHAR(14));
+
+CREATE TABLE enderecos(
+	id INTEGER PRIMARY KEY IDENTITY(1,1), -- PK
+	id_cliente INTEGER NOT NULL, -- FK
+	cep VARCHAR(9) NOT NULL,
+	estado VARCHAR(2) NOT NULL,
+	cidade VARCHAR(30) NOT NULL,
+	bairro VARCHAR(30) NOT NULL,
+	logradouro VARCHAR(40) NOT NULL,
+	numero VARCHAR(10) NOT NULL,
+	descricao TEXT
+
+-- FOREIGN KEY nome_coluna_tabela_atual REFERENCES nome_tabela_pai(nome_coluna_pai_pk)
+-- REFERENCES nome_tabela_pai(nome_coluna_pai_pk)
+
+	FOREIGN KEY(id_cliente) REFERENCES clientes(id));
+
+	
+
+
+
+CREATE TABLE pedidos(
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+	id_cliente INTEGER NOT NULL,
+	status TINYINT NOT NULL, -- 0..255,
+	data_efetivacao_compra DATETIME2,
+	data_criacao DATETIME2 NOT NULL,
+);
+
+CREATE TABLE pedidos_pecas(
+	id INTEGER PRIMARY KEY IDENTITY(1,1), -- PK
+	id_peca INTEGER, -- FK tabela peças
+	id_pedido INTEGER, -- FK tabela pedido
+
+	quantidade SMALLINT,
+
+	FOREIGN KEY(id_pedido) REFERENCES pedidos(id),
+	FOREIGN KEY(id_peca) REFERENCES pecas(id),
+);
+
+
+INSERT INTO pecas(nome, tipo, preco_unitario) VALUES
+('SSD 240 GB', 1, 200),
+('SSD 240 M2', 1, 420.39),
+('RTX 3090 TI', 2, 9999.90),
+('GTX 1060', 2, 1500),
+('16GB DDR5 4800Ghz', 5, 800),
+('16GB DDR4 3200Ghz', 5, 350);
+
+INSERT INTO clientes(nome, cpf) VALUES
+('Claudio', '070.355.489_73'),
+('Cry', '032.599,384-69'),
+('Júliana', '123.495.392-02');
+
+INSERT INTO enderecos(id_cliente, estado, cidade, bairro, cep, logradouro, numero)
+	VALUES
+	(1, 'SC', 'Blumenau', 'Velha', '89070-301', 'Rua Divinapolis', 777),
+	(2, 'SC', 'Blumenau', 'Velha Grande', '89070-472', 'Morro da Edith', 36),
+	(3, 'SC', 'Blumenau', 'Judity', 'Sem cep', 'Rua Kellen', 23);
+
+ SELECT * FROM pecas;
+ SELECT * FROM clientes;
+ SELECT * FROM enderecos;
+
+
+ -- Consulta para listar o nome, cpf e endereço completo do cliente
+ -- Consulta parte da tabela de clientes fazendo INNER JOIN com a tabela de endereço
+ SELECT
+	c.nome,
+	c.cpf,
+	CONCAT(
+		e.estado, ' - ',
+		e.cidade, ' - ',
+		e.bairro, ' - ',
+		e.logradouro, ' ',
+		e.numero, ' - ') AS 'Endereco Completo'
+	FROM clientes AS c
+	INNER JOIN enderecos AS e ON (e.id_cliente = c.id);
+
+-- Consultar os endereços apresentando seu cliente filtrando
+-- por bairros com nome 'Velha'
+-- Consulta partindo da tavbela de endereços com outra consulta na tabela de clientes
+SELECT
+	c.nome,
+	e.bairro,
+	e.logradouro
+	FROM enderecos AS e
+	INNER JOIN clientes AS c ON(c.id = e.id_cliente)
+	WHERE e.bairro LIKE '%Velha%';
+
+-- STATUS do pedidos
+-- 0 - carrinho
+-- 1 - Aguardando pagamento
+-- 2 - Pagamento efetivado
+-- 3 - Entrega realizada
+INSERT INTO pedidos (id_cliente, status, data_criacao) VALUES
+(1, 0, GETDATE()), -- pedido para o Cláudio
+((SELECT id FROM clientes WHERE nome = 'Cry'), 0, GETDATE()); -- Pedido para o Cry
+
+-- Consultar pedidos listando, data de criação formato PT-BR, status com texto
+-- e nome do cliente
+-- Consulta partindo da tabelade pedidos com JSON em clientes
+SELECT 
+	FORMAT(p.data_criacao, 'dd/MM/yyyy'),
+	CASE
+		WHEN [status] = 0 THEN 'Carrinho'
+		WHEN [status] = 1 THEN 'Aguardando pagamento'
+		WHEN [status] = 2 THEN 'Pagamento efetivado'
+		ELSE 'Entrega realizada'
+	END AS 'Status pedido',
+	c.nome
+	FROM pedidos AS p
+	INNER JOIN clientes AS c ON(p.id_cliente = c.id);
+UPDATE pedidos SET status = 1 WHERE id = 2; 
+
---
+++ ExemplosSQLComandosBasicos.sql
@@ -0,0 +1,102 @@
+﻿CREATE TABLE trabalhos(
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+	titulo VARCHAR(100),
+	subtitulo VARCHAR(50), -- armazenár até 50 caracteres
+	descricao TEXT, -- armazenar textos grandes
+	data_publicacao DATETIME2, -- data e hora
+	tipo VARCHAR(15)
+);
+
+INSERT INTO trabalhos
+	(titulo, subtitulo, descricao, data_publicacao, tipo)
+	VALUES ('A melhor pizza do mundo', 
+		'Como degustar uma pizza de calabresa',
+		'Grande descrição aki', '1994-06-04', 'Rceita');
+
+INSERT INTO trabalhos
+	(titulo, subtitulo, descricao, data_publicacao, tipo)
+	VALUES ('Clean code', 'Como trabalhar melhor no seu dia a dia', 
+	'Ultilizar var no lugar de tipo primitivos, Identar seu código,
+	Ultilizar constante', '2000-03-20', 'Leitura técnica');
+
+INSERT INTO trabalhos
+	(titulo, subtitulo, descricao, data_publicacao, tipo)
+	VALUES ('Pudim com ovo', 'Aprenda o que não fazer na sua vida',
+	'Jogue o pudim no lixo e faça um ovo', '2022-07-06', 'Leitura técnica');
+
+INSERT INTO trabalhos
+	(titulo, subtitulo, descricao, data_publicacao, tipo)
+	VALUES ('Estrutura de dados',
+	'como sofrer sem saber, porém é necessário',
+	'Iportante para aprender fifo, lifo, Listas encadeadas',
+	'1980-05-11', 'Leitura técnica');
+
+INSERT INTO trabalhos
+	(titulo, subtitulo, descricao, data_publicacao, tipo)
+	VALUES ('Strogonoff de frango', 'a vitória da batata palha',
+	'Compre frango e batata palha, jogue na panela, acenda o fogão,
+	espera 5 minutinhos e ta plonto o sorvetinho',
+	'2000-09-10', 'Receita');
+
+SELECT id, titulo, subtitulo, descricao, data_publicacao, tipo
+	FROM trabalhos;
+
+SELECT 
+	CONCAT (titulo, ' - ', subtitulo) AS 'Título completo'
+	FROM trabalhos;
+
+-- Consular a descrição limitando a quantidade de caracteres
+SELECT
+	titulo AS 'Título',
+	CONCAT(SUBSTRING(descricao, 0, 30), '...') AS 'Descrição'
+	FROM trabalhos;
+
+-- Apresentar os trabalhos filtrando por ano da publicação entre 1990 até 2010
+SELECT id, titulo, data_publicacao
+	FROM trabalhos
+	WHERE
+		YEAR(data_publicacao) >= 1990 AND
+		YEAR(data_publicacao) <= 2010;
+
+-- Consultar data publicação formato BR huehue
+-- BR: dias/mes/ano
+ SELECT id, titulo,
+	CONCAT(
+		DAY(data_publicacao), '/',
+		MONTH(data_publicacao), '/',
+		YEAR(data_publicacao))
+	FROM trabalhos;
+
+-- Consultar data publicação formatando no padrão BR, ultilizando função para isso
+SELECT id, titulo, FORMAT(data_publicacai, 'dd/MM/yyyy')
+	FROM trabalhos;
+
+SELECT id, titulo FROM trabalhos;
+
+-- Limitar a consulta para dois registros
+SELECT TOP(2) id, titulo FROM trabalhos;
+
+-- Apresentar o código, nome, preço, se está favoritado
+-- filtrando por Placa mãe ou placa de vídeo do maior preço para o menor
+-- SELECT TOP(20) id, nome, preco, favoritado
+-- FROM produtos
+-- WHERE categoria = 'Placa mãe' OR categoria = 'placa de video'
+-- ORDER BY preco DESC
+
+-- Consulta com paginação apresentando 2 registros da página 1
+SELECT TOP(2) id, titulo
+	FROM trabalhos
+	ORDER BY id
+	OFFSET 0 ROWS FETCH NEXT 2 ROWS ONLY;
+
+-- Consulta com paginação apresentando 2 registros da página 2
+SELECT TOP(2) id, titulo
+	FROM trabalhos
+	ORDER BY id
+	OFFSET 2 ROWS FETCH NEXT 2 ROWS ONLY;
+
+-- Consulta com paginação apresentando 2 registros da página 3
+SELECT TOP(2) id, titulo
+	FROM trabalhos
+	ORDER BY id
+	OFFSET 4 ROWS FETCH NEXT 2 ROWS ONLY;
